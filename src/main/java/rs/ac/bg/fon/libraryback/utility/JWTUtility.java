eb3573e1aaeb822a60d9802049fa1792cb6c8d18
dragana <stefanoviic.dragana@gmail.com>
2022-05-29 12:18:06 +0200

    Refactor: Before merge.

--- src/main/java/rs/ac/bg/fon/libraryback/utility/JWTUtility.java
+++ src/main/java/rs/ac/bg/fon/libraryback/utility/JWTUtility.java
@@ -45,6 +45,8 @@ public class JWTUtility implements Serializable {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
              .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+               // .setExpiration(new Date(System.currentTimeMillis() + 1000 * 10))
+
 
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
